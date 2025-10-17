190960d210454b94c540589bb8a635a25ffd90e3
MumtazAysha <mumtazaysha9@gmail.com>
2025-10-17 16:05:54 +0530

    Started creating the data transformation file.

---
+++ src/data/transform.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np
+import logging
+from pathlib import Path
+from src.common.config import load_config
+from src.common.io_utils import read_parquet, write_parquet
+
+
