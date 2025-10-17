fa1313c427f267e8deb9a190020b7e7459c31788
MumtazAysha <mumtazaysha9@gmail.com>
2025-10-17 09:38:37 +0530

    Edited the utilities file.

--- src/common/io_utils.py
+++ src/common/io_utils.py
@@ -20,3 +20,18 @@ def write_parquet(df, path):
     logger.info(f"Writing to {path}")
     df.to_parquet(path, index=False, compression='snappy')
     logger.info(f" Saved {len(df)} rows to {path}")
+
+def read_csv(path):
+    """Read CSV file and return DataFrame"""
+    logger.info(f"Reading {path}")
+    df = pd.read_csv(path)
+    logger.info(f"loaded {len(df)} rows to {path}")
+    return df
+
+def write_csv(df,path):
+    """Write DataFrame to csv file"""
+    path(path).parent.mkdir(parents=True. exist_ok=True)
+    logger.info(f"Writing to {path}")
+    df.to_csv(path, index=False)
+    logger.info(f" saved {len(df)} rows to {path}")
+    
