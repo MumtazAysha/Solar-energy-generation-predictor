28750d9ebb275f3ce5020e8b4fc7ab64bf426acb
MumtazAysha <mumtazaysha9@gmail.com>
2025-10-21 14:12:45 +0530

    Fixed some errors

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -9,6 +9,8 @@ import logging
 import joblib
 from pathlib import Path
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
