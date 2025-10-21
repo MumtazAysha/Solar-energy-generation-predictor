01f95efbc77ee9b15a7e44fd7586dc931d79d172
MumtazAysha <mumtazaysha9@gmail.com>
2025-10-21 09:49:06 +0530

    Started the model training process.

---
+++ src/models/train.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import logging 
+import joblib
+from pathlib import Path
+from sklearn.ensemble import RandmForeestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import warnings
+warnings.filterwarnings('ignore')
+
+from src.common.config import load_config
+from src.common.io_utils import read_parquet
+
+logger = logging.getLogger(__name__)
+
